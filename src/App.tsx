8aef50e7a10e3a788f53b0a858415c86aa64290e
Scott Lackey <thomasscottlackey@gmail.com>
2019-03-06 14:45:44 -0600

    Moved nav items into a data folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { Header, Container, Grid, Segment, Menu } from "semantic-ui-react";
+import { Header, Container, Grid, Segment, Menu, MenuMenuProps } from "semantic-ui-react";
 import Home from "./Screens/Home/Home";
 import About from "./Screens/About/About";
 import Armory from "./Screens/Armory/Armory";
@@ -11,47 +11,33 @@ import Contact from "./Screens/Contact/Contact";
 import Events from "./Screens/Events/Events";
 import Members from "./Screens/Members/Members";
 import _findIndex from "lodash/findIndex";
-interface IProps {
-  activeItem: string;
-}
-
-interface IState {
-  activeItem: string;
+import NavigationMenuData from './Data/NavigationMenu';
+interface State {
+  activeItemIndex: number;
   loaded: boolean;
+  navItems: MenuMenuProps[]
 }
 
-class App extends Component<{}, IState> {
-  loaded = false;
+class App extends Component<{}, State> {
+  public state: State = {
+    loaded: false,
+    navItems: NavigationMenuData,
+    activeItemIndex: _findIndex(NavigationMenuData, { href: location.pathname }),
+  }
 
   componentDidMount(){
     this.setState({loaded: true});
   }
 
   render() {
-    const navItems = [
-      {
-        key: "home",
-        name: "Home",
-        href: "/"
-      },
-      { key: "about", name: "About", href: "/about" },
-      { key: "armory", name: "Armory", href: "/armory" },
-      {
-        key: "chainOfCommand",
-        name: "Chain of Command",
-        href: "/chain-of-command"
-      },
-      { key: "contact", name: "Contact", href: "/contact" },
-      { key: "events", name: "Events", href: "/events" },
-      { key: "members", name: "Members", href: "/members" }
-    ];
-    const activeItemIndex = _findIndex(navItems, { href: location.pathname });
-    return (
+
+
+    return !this.state.loaded ? '' : (
       <Container>
         <Router>
           <Container>
               <Header as="h1">Violent Response Airsoft</Header>
-              <Menu activeIndex={activeItemIndex} items={navItems} />
+              <Menu activeIndex={this.state.activeItemIndex} items={this.state.navItems} />
             <Route path="/" exact component={Home} />
             <Route path="/about" component={About} />
             <Route path="/armory" component={Armory} />
---
+++ src/Data/NavigationMenu.ts
@@ -0,0 +1,17 @@
+export default [
+  {
+    key: "home",
+    name: "Home",
+    href: "/"
+  },
+  { key: "about", name: "About", href: "/about" },
+  { key: "armory", name: "Armory", href: "/armory" },
+  {
+    key: "chainOfCommand",
+    name: "Chain of Command",
+    href: "/chain-of-command"
+  },
+  { key: "contact", name: "Contact", href: "/contact" },
+  { key: "events", name: "Events", href: "/events" },
+  { key: "members", name: "Members", href: "/members" }
+];
